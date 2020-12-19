099baa1295c8e1babf4c7317ddba6fb7e4aa5d37
hellokena <42730820+hellokena@users.noreply.github.com>
2020-12-19 23:57:39 +0900

    12/19

---
+++ 입출력과 사칙연산/2558_곱셈.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = input()
+
+print(a*int(b[2]))
+print(a*int(b[1]))
+print(a*int(b[0]))
+print(a*int(b))
+
+'''
+	int first_digit = first_num*(second_num/100);
+	int second_digit = first_num*(second_num/10%10);
+	int third_digit = first_num*(second_num%10);
+'''
