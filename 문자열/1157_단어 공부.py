8c34cc31de9f6806056f6a71d6e9d089f2e185a5
hellokena <42730820+hellokena@users.noreply.github.com>
2021-04-07 01:29:09 +0900

    4/7 update by dict

--- 문자열/1157_단어 공부.py
+++ 문자열/1157_단어 공부.py
@@ -1,12 +1,19 @@
-string = input().lower()
-
-alpha = [str(i) for i in range(97, 123)]
-num =  [j for j in range(27)]
-
-for i in string:
-    for j in alpha:
-        if j == i :
-
+s = input().upper()
+dic = {}
+for i in s:
+  if i in dic.keys():
+    dic[i] += 1
+  else:
+    dic[i] = 1
+max_v = max(dic.values())
+result = ''
+for key, value in dic.items():
+  if value == max_v:
+    result += key
+if len(result) == 1:
+  print(result[0])
+else:
+  print('?')
 
 
 
