bdf666af48c6a0b08281f18ad3a39d67d32f705a
hellokena <42730820+hellokena@users.noreply.github.com>
2021-04-19 01:38:05 +0900

    Create BOJ#2750_수 정렬하기.py

---
+++ 정렬/BOJ#2750_수 정렬하기.py
@@ -0,0 +1,12 @@
+import sys
+
+def solution(n):
+    array = []
+    for _ in range(n):
+        array.append(int(sys.stdin.readline().rstrip()))
+    array.sort()
+    for i in array:
+        print(i)
+
+n = int(sys.stdin.readline().rstrip()) # 구하고자 하는 수
+solution(n)
